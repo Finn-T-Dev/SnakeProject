f2b91b4da10c7ee23a61eae9918a4ca5abf91130
FINN TUCKER <finn.tucker@hotmail.com>
2025-10-16 16:29:15 +1100

    add enum to handle snake's direction. next step is to limit movement to available directions based on current direction

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,18 @@
 // ===================================================================
 // type definitions for Snake and Food
 // ===================================================================
+static enum directionStates {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT
+};
+
 typedef struct Snake {
-	std::deque<Rectangle> bodyDeque;
-	Vector2 speed;
-	Color color;
+	std::deque<Rectangle> bodyDeque;	// store the snake's body as a deque
+	Color color;						// color (colour) of the snake 
+	directionStates direction;
+	Vector2 velocity;					// speed and direction of travel
 } Snake;
 
 typedef struct Food {
@@ -30,30 +38,27 @@ static const int windowHeight = 800;
 
 static bool bIsGameOver = false;
 static bool bIsPaused = false;
-static int playerScore;
+static int playerScore = 0;
+static int frameCounter = 0;
+static const int updateRate = 30;
 
-static Food fruit = { 0 };
-static std::deque<Snake> snake = {};
+static Snake snake;
+static Food food;
 
 // ===================================================================
 // local forward declarations
 // ===================================================================
-Snake initSnake(Vector2 startPos, float blockSize, Color color)
-{
-	Snake snake;
-	snake.color = color;
-	snake.speed = { blockSize, 0 };
+// initialisation functions
+static Snake initSnake();
+static Food initFood();
+static void initGame();
 
-	Rectangle head = { startPos.x, startPos.y, blockSize, blockSize };
-	snake.bodyDeque.push_back(head);
-
-	return snake;
-}
-static void HandleUserInput(Vector2& playerPos);
-static void SetPlayerPosition(Rectangle& player, Vector2& playerPos);
-static void DrawGrid();
-static void SetNewFoodLocation(Rectangle& food);
-static void UpdateMovingTextPos(Vector2& textPos);
+// game loop functions
+static void drawGame();
+static void drawSnake(const Snake& snake);
+static void drawGrid();
+static void getMovementDirection(Snake& snake);
+static void MoveSnake(Snake& snake, bool grow);
 
 // ===================================================================
 // main entry point
@@ -64,64 +69,46 @@ int main()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create window and OpenGL context
-	InitWindow( windowWidth, windowHeight, "Snake Game");
+	InitWindow(windowWidth, windowHeight, "Snake Game");
 
-	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
-	SearchAndSetResourceDir("resources");
+	//// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
+	//SearchAndSetResourceDir("resources");
 
 	int num_rows = windowHeight / TILE_SIZE;
 	int num_cols = windowWidth / TILE_SIZE;
 
-	Vector2 textPos = { 0.0f, 0.0f };
-
-	Rectangle player = { (windowWidth / 2) - (TILE_SIZE / 2), windowHeight / 2 - (TILE_SIZE / 2), TILE_SIZE, TILE_SIZE };
-	Vector2 playerPos = { player.x, player.y };
-	Rectangle food = { GetRandomValue(0, windowWidth), GetRandomValue(0, windowHeight), TILE_SIZE, TILE_SIZE };
-	Vector2 foodPos = { food.x, food.y };
-
 	int currentFps = 60;
 	SetTargetFPS(currentFps);
-	float moveDelay = 0.25f;
-	float moveTimer = 0.0f;
+
+	initGame();
 
 	// game loop
 	while (!WindowShouldClose())
 	{
-		if (!bIsPaused)
+		if (!bIsGameOver)
 		{
-			moveTimer += GetFrameTime();
+			// toggle pause
+			if (GetKeyPressed() == KEY_P)
+				bIsPaused = !bIsPaused;
 
-			UpdateMovingTextPos(textPos);
-
-			if (moveTimer >= moveDelay)
+			if (!bIsPaused)
 			{
-				moveTimer = 0.0f;
-				SetPlayerPosition(player, playerPos);
-				player.x = playerPos.x;
-				player.y = playerPos.y;
+				frameCounter ++;
+				getMovementDirection(snake);
+				if (frameCounter >= updateRate)
+				{
+					frameCounter = 0;
+					std::cout << "updating snake position" << std::endl;
+					// move the snake
+					MoveSnake(snake, false);
+
+					// check for collision with wall -> if true, game over
+					// check for collision with own body -> if true, game over
+					// check for collision with fruit -> if true, increase score and add segment to body
+
+				}
+				drawGame();
 			}
-			bool bCollidedWithFood = CheckCollisionRecs(player, food);
-
-			if (bCollidedWithFood)
-			{
-				playerScore += 100;
-				std::cout << "PLAYER SCORE:" << playerScore << std::endl;
-				SetNewFoodLocation(food);
-			}
-		}
-		BeginDrawing();
-		ClearBackground(BLACK);
-		DrawGrid();
-		DrawText("Hello", textPos.x, textPos.y, 20, WHITE);
-		HandleUserInput(playerPos);
-		DrawRectangleRec(food, RED);
-		DrawRectangleRec(player, DARKBLUE);
-		DrawText(("Score: " + std::to_string(playerScore)).c_str(), 20, 20, 30, WHITE);
-
-		if (bIsPaused)
-		{
-			DrawText("PAUSED", 640, 400, 40, WHITE);
-
 		}
 		EndDrawing();
 	}
@@ -129,47 +116,84 @@ int main()
 	return 0;
 }
 
-void HandleUserInput(Vector2& playerPos)
-{
-	if (IsKeyDown(KEY_RIGHT)) playerPos.x += 5.0f;
-	if (IsKeyDown(KEY_LEFT)) playerPos.x -= 5.0f;
-	if (IsKeyDown(KEY_UP)) playerPos.y -= 5.0f;
-	if (IsKeyDown(KEY_DOWN)) playerPos.y += 5.0f;
-	if (IsKeyPressed(KEY_P)) bIsPaused = !bIsPaused;
-}
-
-static void SetPlayerPosition(Rectangle& player, Vector2& playerPos)
-{
-	player.x = playerPos.x;
-	player.y = playerPos.y;
-}
-
-void SetNewFoodLocation(Rectangle& food)
-{
-	food.x = static_cast<float>(GetRandomValue(0, windowWidth));
-	food.y = static_cast<float>(GetRandomValue(0, windowHeight));
-}
-
-void UpdateMovingTextPos(Vector2& textPos)
-{
-	textPos.x += int(1);
-	textPos.y += int(1);
-	if (textPos.x >= windowWidth)
+static void getMovementDirection(Snake& snake) {
+	if (IsKeyPressed(KEY_UP))
 	{
-		textPos.x = 0;
+		std::cout << "INPUT::KEY_UP" << std::endl;
+		snake.direction = UP;
 	}
-	if (textPos.y >= windowHeight)
+	else if (IsKeyPressed(KEY_DOWN))
 	{
-		textPos.y = 0;
+		std::cout << "INPUT::KEY_DOWN" << std::endl;
+		snake.direction = DOWN;
+	}
+	else if (IsKeyPressed(KEY_LEFT))
+	{
+		std::cout << "INPUT::KEY_LEFT" << std::endl;
+		snake.direction = LEFT;
+	}
+	else if (IsKeyPressed(KEY_RIGHT))
+	{
+		std::cout << "INPUT::KEY_RIGHT" << std::endl;
+		snake.direction = RIGHT;
 	}
 }
 
-void DrawGrid()
+static void drawGame() {
+	BeginDrawing();
+	ClearBackground(BLACK);
+
+	drawGrid();
+	drawSnake(snake);
+}
+
+static void MoveSnake(Snake& snake, bool grow = false) {
+	switch (snake.direction)
+	{
+	case UP:
+		snake.velocity = { 0, -TILE_SIZE };
+		break;
+
+	case DOWN:
+		snake.velocity = { 0, TILE_SIZE };
+		break;
+
+	case LEFT:
+		snake.velocity = { -TILE_SIZE, 0 };
+		break;
+
+	case RIGHT:
+		snake.velocity = { TILE_SIZE, 0 };
+		break;
+	}
+	// Copy current head
+	Rectangle newHead = snake.bodyDeque.front();
+
+	// Move head by speed
+	newHead.x += snake.velocity.x;
+	newHead.y += snake.velocity.y;
+
+	// Add new head to front
+	snake.bodyDeque.push_front(newHead);
+
+	// Remove tail unless we're growing
+	if (!grow) {
+		snake.bodyDeque.pop_back();
+	}
+}
+
+void drawSnake(const Snake& snake) {
+	for (const auto& segment : snake.bodyDeque) {
+		DrawRectangleRec(segment, snake.color);
+	}
+}
+
+void drawGrid()
 {
 	int squareSize = TILE_SIZE;
-	for (int i = 0; i < GetScreenWidth()/16; i++)
+	for (int i = 0; i < GetScreenWidth() / 16; i++)
 	{
-		for (int j = 0; j < GetScreenHeight()/10; j++)
+		for (int j = 0; j < GetScreenHeight() / 10; j++)
 		{
 			if (i % 2 == 0)
 			{
@@ -193,4 +217,30 @@ void DrawGrid()
 			}
 		}
 	}
+}
+
+static void initGame() {
+	std::cout << "Initialising game" << std::endl;
+	snake = initSnake();
+	food = initFood();
+}
+
+static Snake initSnake() {
+	std::cout << "Initialising snake" << std::endl;
+	Snake s;
+	s.color = YELLOW;
+	s.velocity = { TILE_SIZE, 0 }; // start moving right
+	Rectangle head = { 5 * TILE_SIZE, 5 * TILE_SIZE, TILE_SIZE, TILE_SIZE };
+	s.bodyDeque.push_back(head);
+	return s;
+}
+
+static Food initFood() {
+	std::cout << "Initialising food" << std::endl;
+	Food f;
+	f.size = { TILE_SIZE, TILE_SIZE };
+	f.position = { 10 * TILE_SIZE, 10 * TILE_SIZE };
+	f.active = true;
+	f.color = RED;
+	return f;
 }
